ad4107b473750c49024e5e3c69089dc8db2d20b5
yanyb <simpleyanb@gmail.com>
2024-04-16 12:17:15 +0800

    并发问题

--- utils/random/random.go
+++ utils/random/random.go
@@ -2,25 +2,36 @@ package random
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
 
-var r *rand.Rand
+var (
+	r  *rand.Rand
+	rl *sync.Mutex
+)
 
 func init() {
 	source := rand.NewSource(time.Now().UnixNano())
 	r = rand.New(source)
+	rl = &sync.Mutex{}
 }
 
 func Choice[T any](s []T) T {
+	rl.Lock()
+	defer rl.Unlock()
 	l := len(s)
 	return s[r.Intn(l)]
 }
 
 func RandInt(i, j int) int {
+	rl.Lock()
+	defer rl.Unlock()
 	return i + r.Intn(j-i)
 }
 
 func Random() float64 {
+	rl.Lock()
+	defer rl.Unlock()
 	return r.Float64()
 }
